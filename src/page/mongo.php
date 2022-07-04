bb7f4803ea6287e2c5d9d51f27cac0106ae8b176
Lauri Ojansivu <x@xet7.org>
2022-07-04 09:41:13 +0300

    MongoDB code works now.

--- src/page/mongo.php
+++ src/page/mongo.php
@@ -2,15 +2,24 @@
 
 <?php
 
-require '../vendor/autoload.php';
+require_once 'vendor/autoload.php';
+require_once 'vendor/mongodb/mongodb/src/functions.php';
+
+//require(__DIR__."../vendor/autoload.php");
+use MongoDB\Client as MGDClient;
+
+$collection = (new MGDClient("mongodb://127.0.0.1:27019/wekan"))->wekan->users;
+
+//$cursor = $collection->find([],['limit'=>5]);
+
+//var_dump($cursor);
 
 
 
-$client = new MongoDB\Client("mongodb://localhost:27019");
-$collection = $client->wekan->users;
+//$client = new MongoDB\Client("mongodb://localhost:27019");
+//$collection = $client->wekan->users;
 
-//$result = $collection->find( [ 'name' => 'Hinterland', 'brewery' => 'BrewDog' ] );
-$result = $collection->findOne()->toArray();
+$result = $collection->findOne();
 
 print_r($result);
 
