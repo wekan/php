f45e2fd252e4069c7c7c5bc9c9b76ce6e520cc82
Lauri Ojansivu <x@xet7.org>
2022-09-12 06:29:01 +0300

    Show all upload options visible anyway, so that it can be tried what works on e-ink bookeen reader browser.

--- src/page/dragupload.php
+++ src/page/dragupload.php
@@ -1,21 +1,18 @@
 
-<script>
-// Write drag upload form for Javascript browsers
-document.write(`
-    <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="post" enctype="multipart/form-data">
-    <div id="drop_file_zone" ondrop="upload_file(event)" ondragover="return false">
-        <div id="drag_upload_file">
-            <p>Drop file here</p>
-            <p>or</p>
-            <p><input type="button" value="Select File" onclick="file_explorer();" /></p>
-            <input type="file" id="selectfile" />
-        </div>
+<form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="post" enctype="multipart/form-data">
+<div id="drop_file_zone" ondrop="upload_file(event)" ondragover="return false">
+    <div id="drag_upload_file">
+        <p>Drop file here</p>
+        <p>or</p>
+        <p><input type="button" value="Select File" onclick="file_explorer();" /></p>
+        <input type="file" id="selectfile" />
     </div>
-    <div class="img-content"></div>
+</div>
+<div class="img-content"></div>
 
-    </form>
-`);
+</form>
 
+<script>
 var fileobj;
 function upload_file(e) {
     e.preventDefault();
@@ -77,10 +74,10 @@ function ajax_file_upload(file_obj) {
 }
 </script>
 
-<noscript>
+<hr>
+
 <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="post" enctype="multipart/form-data">
   Select image to upload:
   <input type="file" name="fileToUpload" id="fileToUpload">
   <input type="submit" value="Upload Image" name="submit">
 </form>
-</noscript>
