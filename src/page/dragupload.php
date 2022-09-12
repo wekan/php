b1c9ef991badeded20155b26e840dc9620817011
Lauri Ojansivu <x@xet7.org>
2022-09-12 06:34:46 +0300

    Hide confusing double upload.

--- src/page/dragupload.php
+++ src/page/dragupload.php
@@ -2,10 +2,13 @@
 <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="post" enctype="multipart/form-data">
 <div id="drop_file_zone" ondrop="upload_file(event)" ondragover="return false">
     <div id="drag_upload_file">
-        <p>Drop file here</p>
-        <p>or</p>
-        <p><input type="button" value="Select File" onclick="file_explorer();" /></p>
-        <input type="file" id="selectfile" />
+        <p>Drop file here to upload</p>
+<?php
+// Hide confusing double upload        
+//        <p>or</p>
+//        <p><input type="button" value="Select File" onclick="file_explorer();" /></p>
+//        <input type="file" id="selectfile" />
+?>        
     </div>
 </div>
 <div class="img-content"></div>
